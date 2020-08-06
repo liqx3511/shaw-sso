78d677457471204d3ce9b4f504ab15e053f93f07
李庆肖 <15008139511@163.com>
2020-08-06 17:30:16 +0800

    2020-8-6 分支测试1

---
+++ src/main/java/测试/Test1.java
@@ -0,0 +1,16 @@
+package 测试;
+
+/**
+ * @ProjectName: sso
+ * @Package: 测试
+ * @ClassName: Test1
+ * @Author: liqingxiao
+ * @Description: 测试分支1
+ * @Date: 2020/8/6 17:28
+ * @Version: 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(111111);
+    }
+}
